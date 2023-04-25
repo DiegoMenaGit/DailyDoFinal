851fb4309f0a5bbff34666f724967af6365bb706
Diego Mena <diegoandresmenamuhlhauser@hotmail.com>
2023-04-25 15:44:05 +0200

    añado logo

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ window.onscroll = function() {myFunction()};
 let progress = document.getElementById('progressbar')
 let totalHeghit = document.body.scrollHeight - window.innerHeight;
 
-
 var header = document.querySelector(".header");
+var image = document.querySelector(".image_logo2");
+var imagecontainer = document.querySelector(".image_container2");
 var foto = document.querySelector(".header__container__foto");
 var sticky = header.offsetTop;
 var imagen = document.querySelector(".article__section__img__2");
@@ -14,10 +15,17 @@ function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.remove("header");
     header.classList.add("sticky__header");
+    image.classList.remove("image_logo2");
+    image.classList.add("image_logo");
+    imagecontainer.classList.remove("image_container2");
+    imagecontainer.classList.add("image_container");
     foto.classList.add("fotoPetit");
   } else {
     header.classList.add("header");
     header.classList.remove("sticky__header");
+    image.classList.add("image_logo2")
+    imagecontainer.classList.remove("image_container");
+    imagecontainer.classList.add("image_container2");
     foto.classList.remove("fotoPetit");
   }
   let progressHeight = (window.pageYOffset / totalHeghit) * 200;
