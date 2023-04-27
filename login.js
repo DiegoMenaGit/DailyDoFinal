761b280c043c8e3f17c915df8a69132590674653
Diego Mena <diegoandresmenamuhlhauser@hotmail.com>
2023-04-27 15:31:16 +0200

    formulario

--- login.js
+++ login.js
@@ -1,4 +1,12 @@
 const bubbles = document.querySelector('.bubbles');
+
+var login = document.querySelector(".login")
+var register = document.querySelector(".register")
+var boton = document.querySelector(".boton")
+
+login.classList.add("section__div1")
+register.classList.add("section__div1__transition")
+
 let intervalId = null;
 
 function createSpan() {
@@ -13,3 +21,34 @@ function createSpan() {
 }
 
 intervalId = setInterval(createSpan, 200);
+var clickCount = 0
+
+boton.addEventListener('click', () => {
+    clickCount++;
+    
+
+    if (clickCount % 2 === 0) {
+        register.classList.remove("section__div1")
+        register.classList.add("section__div1__transition")
+        setTimeout(function(){
+        login.classList.add("section__div1")
+        login.classList.remove("section__div1__transition")
+        },1000)
+    } else {
+        login.classList.remove("section__div1")
+        login.classList.add("section__div1__transition")
+        setTimeout(function(){
+
+        register.classList.add("section__div1")
+        register.classList.remove("section__div1__transition")
+        },1000)
+    }
+});
+
+setInterval(function() {
+    if (register.classList.contains('section__div1') && login.classList.contains('section__div1')) {
+        bubbles.classList.add("angry")
+    } else {
+        bubbles.classList.remove("angry")
+    }
+  }, 1000);
