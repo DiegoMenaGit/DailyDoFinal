fcba2995a637285313e948ae628fa3641c2e9868
Diego Mena <diegoandresmenamuhlhauser@hotmail.com>
2023-05-02 11:57:03 +0200

    pesta modal

--- login.js
+++ login.js
@@ -1,6 +1,10 @@
 const bubbles = document.querySelector('.bubbles');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
 
 var active = false;
+
+var mensaje = document.querySelector(".mensaje")
 
 var login = document.querySelector(".login")
 var register = document.querySelector(".register")
@@ -19,6 +23,7 @@ var email2 = document.querySelector(".input_email")
 var username2 = document.querySelector(".username")
 var contrasenya2 = document.querySelector(".password")
 
+var logout = document.querySelector(".logout")
 
 login.classList.add("section__div1")
 register.classList.add("section__div1__transition")
@@ -97,21 +102,56 @@ volver.addEventListener('click', ()=>{
 
 boton2.addEventListener("click", (e)=>{
   if(active){
+    console.log("estas en el register")
   console.log(email.value, username.value, contrasenya.value, repetirContrasenya.value)
   if(contrasenya.value == repetirContrasenya.value){
   auth
       .createUserWithEmailAndPassword(email.value, contrasenya.value)
       .then(userCredential => {
         // clear the form !!!!
+        allInputs.forEach(input => {
+          input.value = "";
+        });
         // irse a otra pagina !!!!
         console.log("sign-up")
       })
   }
   else{
-    alert("¡¡¡Las contraseñas NO coinciden!!!")
+    //alert("¡¡¡Las contraseñas NO coinciden!!!")
+    modal.style.display = 'flex';
+    modalContent.style.display = 'flex';
+    modalContent.innerHTML = "¡¡¡Las contraseñas NO coinciden!!!";
   }
 }
 else{
   console.log("estas en el login")
+  console.log(email2.value, username2.value, contrasenya2.value)
+  auth
+      .signInWithEmailAndPassword(email2.value, contrasenya2.value)
+      .then(userCredential => {
+        // clear the form !!!!
+        allInputs.forEach(input => {
+          input.value = "";
+        });
+        // irse a otra pagina !!!!
+        console.log("sign-in")
+      })
 }
 })
+/*
+logout.addEventListener("click", (e)=>{
+  logout.preventDefault();
+  auth.signOut().then(()=>{
+    console.log("sign out")
+  })
+})
+*/
+
+
+
+modal.addEventListener('click', function(e) {
+  if (e.target === modal || e.target === modalContent) {
+    modal.style.display = 'none';
+    modalContent.style.display = 'none';
+  }
+});
