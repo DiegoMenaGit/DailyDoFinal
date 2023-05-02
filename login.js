d05adf86ba827c66b145f9463c65323a1407ffab
Diego Mena <diegoandresmenamuhlhauser@hotmail.com>
2023-05-02 10:32:26 +0200

    firebase principio

--- login.js
+++ login.js
@@ -1,10 +1,24 @@
 const bubbles = document.querySelector('.bubbles');
 
+var active = false;
+
 var login = document.querySelector(".login")
 var register = document.querySelector(".register")
 var boton = document.querySelector(".boton1__p")
 var boton2 = document.querySelector(".boton")
 var volver = document.querySelector(".volver")
+
+var allInputs = document.querySelectorAll(".text_input")
+
+var email = document.querySelector(".input_email_register");
+var username = document.querySelector(".input_username_register");
+var contrasenya = document.querySelector(".input_password_register");
+var repetirContrasenya = document.querySelector(".input_repeatpassword_register")
+
+var email2 = document.querySelector(".input_email")
+var username2 = document.querySelector(".username")
+var contrasenya2 = document.querySelector(".password")
+
 
 login.classList.add("section__div1")
 register.classList.add("section__div1__transition")
@@ -31,16 +45,25 @@ boton.addEventListener('click', () => {
         register.classList.remove("section__div1")
         register.classList.add("section__div1__transition")
         setTimeout(function(){
-        login.classList.add("section__div1")
-        login.classList.remove("section__div1__transition")
+          active = false;
+          boton.innerHTML = "Register"
+          login.classList.add("section__div1")
+          login.classList.remove("section__div1__transition")
+          allInputs.forEach(input => {
+            input.value = "";
+          });
         },1000)
     } else {
         login.classList.remove("section__div1")
         login.classList.add("section__div1__transition")
         setTimeout(function(){
-
-        register.classList.add("section__div1")
-        register.classList.remove("section__div1__transition")
+          active = true;
+          boton.innerHTML = "Login"
+          register.classList.add("section__div1")
+          register.classList.remove("section__div1__transition")
+          allInputs.forEach(input => {
+            input.value = "";
+          });
         },1000)
     }
 });
@@ -66,4 +89,29 @@ setInterval(function() {
 volver.addEventListener('click', ()=>{
     console.log("Clicker")
     window.location.href = 'index.html';
+})
+
+
+
+
+
+boton2.addEventListener("click", (e)=>{
+  if(active){
+  console.log(email.value, username.value, contrasenya.value, repetirContrasenya.value)
+  if(contrasenya.value == repetirContrasenya.value){
+  auth
+      .createUserWithEmailAndPassword(email.value, contrasenya.value)
+      .then(userCredential => {
+        // clear the form !!!!
+        // irse a otra pagina !!!!
+        console.log("sign-up")
+      })
+  }
+  else{
+    alert("¡¡¡Las contraseñas NO coinciden!!!")
+  }
+}
+else{
+  console.log("estas en el login")
+}
 })
